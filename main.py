00738702b5f054348fe49636119e4673c395eb79
Cesar993 <ce.serrano@duocuc.cl.com>
2024-11-12 18:48:06 -0300

    A

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5173 , https://videoappmusica.netlify.app/"],  # Cambia esto por la URL de tu frontend
+    allow_origins=["http://127.0.0.1:5173", "https://videoappmusica.netlify.app/"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
